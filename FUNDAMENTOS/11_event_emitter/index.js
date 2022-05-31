8f052038ef4cbac98f3bfe6ef15001e4f6a84ad1
alvaro <alvarocondeorlando@gmail.com>
2022-05-31 00:03:48 -0300

    event_emitter

---
+++ FUNDAMENTOS/11_event_emitter/index.js
@@ -0,0 +1,12 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('start', () => {
+    console.log('Durante');
+})
+
+console.log('Antes');
+
+emitter.emit('start');
+
+console.log('Depois');
